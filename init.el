78f6b54655f749788d74eede9d9bec40db445176
黄浩 <rotile@huanghaodeMacBook-Air.local>
2018-06-03 10:12:25 +0800

    add misc feature

---
+++ init.el
@@ -0,0 +1,6 @@
+;;my emacs.d private configuration
+(setq debug-on-error t)
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+;;require
+(require 'init-misc)
---
+++ lisp/init-misc.el
@@ -0,0 +1,16 @@
+
+;;gui setting
+(tool-bar-mode 0) 
+(menu-bar-mode 0) 
+(scroll-bar-mode 0) 
+
+;;startup animation
+(setq inhibit-startup-message t)
+
+;;display time
+(display-time-mode 1) ;; 常显 
+(setq display-time-24hr-format t) ;;格式 
+(setq display-time-day-and-date t) ;;显示时间、星期、日期
+
+;;provide feature
+(provide 'init-misc)
